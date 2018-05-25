787c72196215cd4cbcdb50544e1cba82c6fd7e6d
于露 <>
2018-05-25 16:53:01 +0800

    @test

---
+++ NSString+Empty.h
@@ -0,0 +1,12 @@
+//
+//  NSString+Empty.h
+//  FBSnapshotTestCase
+//
+//  Created by 于露 on 2018/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Empty)
+
+@end
---
+++ NSString+Empty.m
@@ -0,0 +1,12 @@
+//
+//  NSString+Empty.m
+//  FBSnapshotTestCase
+//
+//  Created by 于露 on 2018/5/25.
+//
+
+#import "NSString+Empty.h"
+
+@implementation NSString (Empty)
+
+@end
